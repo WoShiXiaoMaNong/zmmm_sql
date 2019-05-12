a2c4c2ab15a276e0b2fc23634f244e5e519892b0
woshixiaomanong <zhongming139@126.com>
2019-05-12 16:23:30 +0800

    modify table fields

--- src/tables/TRADE.sql
+++ src/tables/TRADE.sql
@@ -1,6 +1,11 @@
 
 create table TRADE(
-    ID              integer,
+    ID              integer NOT NULL AUTO_INCREMENT,
     AMOUNT          float,
     TRADE_DATE      DATE,
-    TRADE_DESC      TEXT);
+    TRADE_DESC      TEXT,
+    STATUS          VARCHAR(10),
+    TRADE_TYPE_ID    VARCHAR(10),
+    CREATED_DATE    TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (ID)
+    );
